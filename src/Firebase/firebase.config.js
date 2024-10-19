11ac83877331276ac55fc58ebb3695dc7f7450a8
Khalil <khalil5614@gmail.com>
2024-10-20 00:39:00 +0600

    env implementation

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,36 +1,17 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBdDLIpW3WieTFDfUYBs1Ka9qAHn5A4xP8",
-//   authDomain: "login-practise-react.firebaseapp.com",
-//   projectId: "login-practise-react",
-//   storageBucket: "login-practise-react.appspot.com",
-//   messagingSenderId: "1040206111131",
-//   appId: "1:1040206111131:web:32f30cd88275ed6ee3813e",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export default app;
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+require("dotenv").config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD99ReiTHI6iez8uPlzrdvfuxa3e1MYqh4",
-  authDomain: "code-cloud-2e220.firebaseapp.com",
-  projectId: "code-cloud-2e220",
-  storageBucket: "code-cloud-2e220.appspot.com",
-  messagingSenderId: "992595783155",
-  appId: "1:992595783155:web:860dfc0f90a024c40e3686",
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
